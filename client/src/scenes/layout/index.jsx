5e82458044d963d198458acda28ebf33d88f7174
Rohit-Mene <rohitmene@gmail.com>
2024-01-15 19:51:44 -0800

    Finished Navbar and Sidebar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,13 +3,25 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { UseSelector } from "react-redux";
 import Navbar from "components/Navbar";
-//import Sidebar from "components/Sidebar";
+import Sidebar from "components/Sidebar";
 const Layout = () => {
-  const isNonMobile = useMediaQuery("(min-width: 600px)")
+  {/**isNonMobile is true if it is a desktop */}
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const[isSidebarOpen,setIsSidebarOpen] = useState(true);
+
   return (
-    <Box width="100%" height="100%">
+    <Box display = {isNonMobile ? "flex":"block"}width="100%" height="100%">
+     <Sidebar 
+        isNonMobile = {isNonMobile}
+        drawerWidth = "250px"
+        isSidebarOpen = {isSidebarOpen}
+        setIsSidebarOpen = {setIsSidebarOpen}
+     />
       <Box>
-        <Navbar />
+        <Navbar 
+          isSidebarOpen = {isSidebarOpen}
+          setIsSidebarOpen = {setIsSidebarOpen}
+        />
         {/** This outlet basically represents the child that will be loaded,based on the page we are, say we are in the dashboard page,so we will load navbar and the outlet will load the dashboard */}
         <Outlet />
       </Box>
