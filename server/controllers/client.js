9ee615e9689da1af46f947fa25bdb366c6c8c002
Rohit-Mene <rohitmene@gmail.com>
2024-01-22 17:33:21 -0800

    Customers Page complete

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js"
 //Find all the products requested, and for each product find the product stat for that product and return the product info and its stat(we use spread operator for that)
 export const getProducts = async (req, res) => {
   try {
@@ -21,3 +22,13 @@ export const getProducts = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const getCustomers = async(req,res)=>{
+  try {
+    //The select makes sure we do not send the password in the API response(which is we dont send the password we have for each user stored in thed database)
+    const customers= await User.find({role:"user"}).select("-password");
+    res.status(200).json(customers)
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
